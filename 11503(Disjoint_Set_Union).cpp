6e335fe613f953fb807c2d308ba63467c4a8aa9d
Rakib Mahmud <31930455+Rakib-Mahmud@users.noreply.github.com>
2018-05-31 11:52:46 +0600

    Disjoint Set Union

---
+++ 11503(Disjoint_Set_Union).cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+#define mx 100001
+
+using namespace std;
+
+vector<int>Rank(mx),parent(mx);
+map<string,int>mp;
+
+void makeset(int value)
+{
+    parent[value]=value;
+    Rank[value]=1;
+}
+
+int findset(int value)
+{
+    if(parent[value]==value){
+        return value;
+    }
+    return parent[value]=findset(parent[value]);
+}
+
+void Union(int x,int y)
+{
+    int parent1=findset(x);
+    int parent2=findset(y);
+
+    if(parent1==parent2){
+        return;
+    }
+
+    if(Rank[parent1]>=Rank[parent2]){
+        Rank[parent1] = Rank[parent1]+Rank[parent2];
+        parent[parent2]=parent1;
+    }
+
+    else{
+        Rank[parent2] = Rank[parent1]+Rank[parent2];
+        parent[parent1]=parent2;
+    }
+}
+
+bool isUnion(int x,int y)
+{
+    return findset(x)==findset(y);
+}
+
+int main()
+{
+    string str[mx],s1,s2;
+    int C,R,a,b,T;
+    scanf("%d",&T);
+    while(T--){
+        C=0;
+        scanf("%d",&R);
+        for(int i=0;i<R;i++){
+            cin>>s1>>s2;
+            if(mp.find(s1)==mp.end()){
+                mp[s1]=++C; makeset(C);
+            }
+            if(mp.find(s2)==mp.end()){
+                mp[s2]=++C; makeset(C);
+            }
+            if(isUnion(mp[s1],mp[s2])){  }
+            else{
+                Union(mp[s1],mp[s2]);
+            }
+            a=findset(mp[s1]);
+            printf("%d\n",Rank[a]);
+        }
+        fill(parent.begin(),parent.end(),0); fill(Rank.begin(),Rank.end(),0);
+        mp.clear();
+    }
+    return 0;
+}
